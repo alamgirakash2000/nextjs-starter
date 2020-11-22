5764c2925506f6502918830a96b97f58777e9a57
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-11-22 20:15:42 +0600

    With bootsytrap

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import "../styles/index.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="home">
-      <h1>My First Next App</h1>
+    <div>
+      <Head>
+        <title>First Next app</title>
+      </Head>
+
+      <main className="home text-center">
+        <h1>My First Next App</h1>
+      </main>
     </div>
   );
 }
