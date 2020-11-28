441c3c9d6b27a56c705fcbbcdfe1e59b8e001b43
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-11-29 01:40:46 +0600

    push again

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Techie Product</title>
-        <meta
-          name="description"
-          content="This is a site for different types of techie products and reviews, also for blog content"
-        />
+        <title>NextJs Starter By Akash</title>
+        <meta name="description" content="NextJs Starter By Akash" />
       </Head>
 
       <main className="home text-center">
         <h1>
-          <i>Techie Product Website will be available soon...</i>
+          <i>NextJs Starter By Akash</i>
         </h1>
         <p></p>
       </main>
